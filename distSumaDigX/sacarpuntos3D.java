f4c3248e3d32a9cb41d916c0567f0a45be9d1f5e
pvtoari <43178469+pvtoari@users.noreply.github.com>
2024-04-09 22:30:01 +0000

    creada version 3D porque soy muy curioso

---
+++ distSumaDigX/sacarpuntos3D.java
@@ -0,0 +1,60 @@
+package distSumaDigX;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.*;
+
+public class sacarpuntos3D implements Serializable {
+
+    public static void main(String[] args) {
+
+        Scanner kbd = new Scanner(System.in);
+        
+        System.out.println("Dominio XY [0,n]: ");
+        final double dom = kbd.nextInt();
+        System.out.println("Z (puntos forma [n, sumD(n), z]): ");
+        final double z = kbd.nextInt();
+
+        String salida = "";
+        String anim= "|/-\\";
+
+        int i = 0;
+        long ti, tf;
+
+        kbd.close();
+
+        try (FileWriter file = new FileWriter("//workspaces//matecosas//distSumaDigX//puntos3D.dat")) {
+
+            ti = System.currentTimeMillis();
+            while(i<=dom) {
+                String data = "\r" + anim.charAt(i % anim.length()) + " " + i + " (" + (System.currentTimeMillis()-ti)/1000d + " s)";
+                System.out.write(data.getBytes());
+                //Thread.sleep(1);
+
+                salida += i + " " + sumaD(i) + " " + z + "\n";
+                i++;
+            }
+            tf = System.currentTimeMillis();
+
+            file.write(salida);
+
+            System.out.println("\nTiempo de calculo y escritura: " + (tf-ti)/1000d + " ms.");
+            System.out.println(i + " puntos generados.");
+            System.out.println("Puntos escritos correctamente.");
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Por las barbas de Merlin, un error ha sucedido: " + e.getMessage());
+        }
+    }
+
+    static int sumaD(int n){
+        if (n <= 9) {
+            return n;
+        }
+        else
+        {
+            return ((n % 10) + sumaD(n/10));
+        }
+    }
+}
