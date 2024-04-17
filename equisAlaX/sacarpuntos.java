ff9ad8366771ada91f0721ddc7b4a57fd85e9333
Ariel Roque <43178469+pvtoari@users.noreply.github.com>
2024-04-17 21:30:58 +0000

    x^x v1

--- equisAlaX/sacarpuntos.java
+++ equisAlaX/sacarpuntos.java
@@ -19,26 +19,39 @@ public class sacarpuntos implements Serializable {
 
         kbd.close();
 
+        long ti, tf, tt;
+        String anim= "|/-\\";
+
         try (FileWriter file = new FileWriter("//workspaces//matecosas//equisAlaX//puntos.dat")) {
 
+            ti = System.currentTimeMillis();
             while (-dom <= x && x <= dom) {
+
+
                 i++;
                 x += dom/divs;
                 Re = Math.cos(Math.PI * x);
                 Im = Math.sin(Math.PI * x);
                 //salida += x + " " + Re + " " + Im + "\n";
-
+                double absX= Math.abs(x);
                 if(x>0) {
-                    salida += x + " " + Math.abs(Math.pow(x,x)); 
-                } else if(x<0){
-                    salida += x + " ";
+                    salida += x + " " + Math.pow(absX, absX) + " " + "0.0" + "\n"; 
+                } else if(x<0) {
+                    salida += x + " " + Math.pow(absX, x)*Re + " " + Math.pow(absX, x)*Im + "\n";
                 }
 
+                String data = "\r" + anim.charAt(i % anim.length()) + " " + i + " (" + (System.currentTimeMillis()-ti)/1000d + " s)";
+                System.out.write(data.getBytes());
             }
+            tf = System.currentTimeMillis();
+
+            tt=tf-ti;
 
             file.write(salida);
-            System.out.println(i + " puntos generados");
-            System.out.println("Escrito socioooooooooo");
+            System.out.println("\n" + i + " puntos generados");
+            System.out.println("Puntos guardados y formateados en /puntos.dat");
+            System.out.println("Tiempo de calculo iterativo (ms) : " + tt + "ms");
+            System.out.println("Tiempo de calculo iterativo (s) : " + tt/1000d + "s");
         } catch (IOException e) {
             //e.printStackTrace();
             System.out.println("error de la hostia: " + e.getMessage());
